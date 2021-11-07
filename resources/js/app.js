768db3ed86eda68599ade2f2235cf212be4e1fd8
Anikreza <tanvirrezaanik@gmail.com>
2021-11-08 00:37:16 +0600

    push from anik

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import Example from "./components/Example";
+
+
+require('./components/Example');
+
+import ReactDOM from 'react-dom'
+import Index from "./components/Example";
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+}
---
+++ resources/js/components/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Example() {
+    return (
+        <div>
+            <h2> Hello Miss Zain</h2>
+        </div>
+    );
+}
+
+export default Example;
+
