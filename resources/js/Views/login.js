bad00f2621d01f155bc27c4c79566f95aec5746d
Anikreza <tanvirrezaanik@gmail.com>
2021-11-17 22:26:54 +0600

    Added home and footer component and with styling: REACT

--- resources/js/Views/login.js
+++ resources/js/Views/login.js
@@ -1,55 +1,51 @@
 import React, {useState} from 'react'
-import { useNavigate } from 'react-router';
+import {useNavigate} from 'react-router';
 import '../../sass/registration.scss'
 
 const Login = () => {
-    const [email, setEmail]=useState();
-    const [password, setPassword]=useState();
-    const navigate=useNavigate();
-    let user=JSON.parse(window.localStorage.getItem('user'));
-    const login=async()=>{
 
-        let user=JSON.parse(window.localStorage.getItem('user'));
-        let userdata={email,password};
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+    let user = JSON.parse(window.localStorage.getItem('user'));
 
-        let API= fetch('http://localhost:8000/api/login',{
-            method:'POST',
-            body:JSON.stringify(userdata),
-            headers:{
-                'Content-Type':'application/json',
-                'Accept':'application/json'
+    const login = async () => {
+        let user = JSON.parse(window.localStorage.getItem('user'));
+        let userdata = {email, password};
+        let API = fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            body: JSON.stringify(userdata),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         })
-
-        API=await (await API).json();
-        window.localStorage.setItem('user',JSON.stringify(API));
-
-        if(!user){
+        API = await (await API).json();
+        window.localStorage.setItem('user', JSON.stringify(API));
+        if (!user) {
             navigate('/');
             window.location.reload();
-        }
-        else{
+        } else {
             navigate('/home');
             window.location.reload();
         }
-
     }
 
     return (
         <div className='login'>
             <div className='login-box'>
-                <input type='text' placeholder='email'  onChange={(e)=>setEmail(e.target.value)}/>
+                <input type='text' placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
                 <br/>
                 <br/>
-                <input type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)}/>
+                <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
                 <br/>
                 <br/>
-                <button style={{backgroundColor:'#859e7e'}} onClick={login} > Sign In </button>
-                <button style={{backgroundColor:'#b58b8b'}} onClick={()=>{navigate('/register')}} > Sign Up </button>
-
+                <button style={{backgroundColor: '#859e7e'}} onClick={login}> Sign In</button>
+                <button style={{backgroundColor: '#b58b8b'}} onClick={() => {
+                    navigate('/register')
+                }}> Sign Up
+                </button>
             </div>
-
-
         </div>
     )
 }
---
+++ resources/js/components/footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../../sass/footer.scss'
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillTwitterSquare } from 'react-icons/ai';
+import { IoLogoYoutube } from 'react-icons/io';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { FaPatreon } from 'react-icons/fa';
+import logo from '../resources/logo.png'
+
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='f-content'>
+
+                <img src={logo} className='footer-logo'/>
+                <div>
+                    <a href='https://www.youtube.com/channel/UC2BR_6GXKLCMX9SEETnO7KQ' target='blank'><IoLogoYoutube className='yticon' size='30px' color='black'/></a>
+                    <a href='https://www.facebook.com/Anikreza22/' target='blank'> <AiFillFacebook className='fbicon' size='30px' color='black'/></a>
+                    <a href='https://www.instagram.com/anikrezain/?hl=en' target='blank'><FaInstagramSquare className='inicon' size='30px' color='black'/></a>
+                    <a href='https://twitter.com/TanvirRezaAnik1' target='blank'> <AiFillTwitterSquare className='ttricon' size='30px' color='black'/></a>
+                    <a href='https://www.patreon.com/user?u=36041123' target='blank'> <FaPatreon className='pticon' size='30px' color='black' /></a>
+                </div>
+                <p1>Copyright ©2021 3dClubBD</p1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
