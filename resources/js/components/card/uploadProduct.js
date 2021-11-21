24e87d4739fbcc9da96a0df43bdd0ed2da02263c
Anikreza <tanvirrezaanik@gmail.com>
2021-11-22 05:24:58 +0600

    added selection option for category table

--- resources/js/components/card/uploadProduct.js
+++ resources/js/components/card/uploadProduct.js
@@ -1,42 +1,39 @@
 import React, {useState, useEffect} from 'react'
 import '../../../sass/upload.scss'
 import TextareaAutosize from 'react-textarea-autosize';
+import { BsImage } from 'react-icons/bs';
 
 const Add = () => {
-    const [name, setName]=useState('');
-    const [stock, setStock]=useState('');
-    const [price, setPrice]=useState('');
-    const [bookType, setBookType]=useState('');
-    const [covertype, setCoverType]=useState([]);
-    const [description, setDescription]=useState('');
-    const [file, setFile]=useState('');
-    const [state, setState]=useState(true);
-    const api=process.env.MIX_API;
+    const [name, setName] = useState('');
+    const [stock, setStock] = useState('');
+    const [price, setPrice] = useState('');
+    const [bookType, setBookType] = useState('A');
+    const [coverType, setCoverType] = useState('B');
+    const [category, setCategory] = useState([]);
+    const [description, setDescription] = useState('');
+    const [file, setFile] = useState('');
+    const api = process.env.MIX_API;
 
-    useEffect(async()=> {
-        let result=await fetch(`${api}/addCategoryType`)
-        let Result= await result.json();
-        setCoverType(Result)
+    useEffect(async () => {
+        let result = await fetch(`${api}/addCategoryType`)
+        let Result = await result.json();
+        setCategory(Result)
+    }, [])
 
-    }, [state])
+    async function upload() {
+        console.log(name, price, description,bookType,coverType,stock,file.name)
+        const Data = new FormData();
+        Data.append('file', file);
+        Data.append('name', name);
+        Data.append('stock', stock);
+        Data.append('price', price);
+        Data.append('bookType', bookType);
+        Data.append('coverType', coverType);
+        Data.append('description', description);
 
-    const getCategory =async(e)=>{
-        setState(false)
-        console.log(covertype[12].coverType)
-    }
-
-    async function upload(){
-        console.log(name, price, description,file.name)
-        const Data= new FormData();
-        Data.append('file',file);
-        Data.append('name',name);
-        Data.append('stock',stock);
-        Data.append('price',price);
-        Data.append('description',description);
-
-        let result=await fetch(`${api}/store`,{
-            method:'POST',
-            body:Data,
+        let result = await fetch(`${api}/store`, {
+            method: 'POST',
+            body: Data,
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
@@ -44,41 +41,51 @@ const Add = () => {
         })
         alert('saved your data')
     }
+
     return (
         <div>
             <div className='add-page'>
-                <input type='text' placeholder='name' name='name' onChange={(e)=>setName(e.target.value)}/>
+                <input type='text' placeholder='name' name='name' onChange={(e) => setName(e.target.value)}/>
                 <br/>
                 <br/>
-                <select>
-                    <option value="A">Apple</option>
+                <label htmlFor="book">Choose Book Genre:</label>
+                <select id='book' name='bookType' onChange={(e)=>setBookType(e.target.value)}>
+                    {
+                        category.map((data) => (
+                            <option>
+                                {data.bookType}
+                            </option>
+                        ))
+                    }
                 </select>
                 <br/>
                 <br/>
-                <select value='Selet Type' onChange={getCategory}>
-                    <option value='1'>{covertype.map((data)=>(
-                        data.coverType
-                        ))}</option>
-                    <option> 'aaaa'</option>
+                <label for="cover">Choose Cover type:</label>
+                <select id='cover' name='coverType' onChange={(e)=>setCoverType(e.target.value)}>
+                    {
+                        category.map((data) => (
+                            <option>
+                                {data.coverType}
+                            </option>
+                        ))
+                    }
                 </select>
                 <br/>
                 <br/>
-                <input type='text' placeholder='cover' name='coverType' onChange={(e)=>setCoverType(e.target.value)}/>
+                <input type='text' placeholder='stock' name='stock' onChange={(e) => setStock(e.target.value)}/>
                 <br/>
                 <br/>
-                <input type='text' placeholder='stock' name='stock' onChange={(e)=>setStock(e.target.value)}/>
+                <label htmlFor="image"><BsImage size={20} style={{cursor:"pointer"}}/> Upload Product Image</label>
+                <input  style={{display:"none"}}   id='image' type='file' placeholder='file' name='file' onChange={(e) => setFile(e.target.files[0])}/>
                 <br/>
                 <br/>
-                <input type='file' placeholder='file' name='file' onChange={(e)=>setFile(e.target.files[0])}/>
-                <br/>
-                <br/>
-                <input type='text'  placeholder='price' name='price' onChange={(e)=>setPrice(e.target.value)}/>
+                <input type='text' placeholder='price' name='price' onChange={(e) => setPrice(e.target.value)}/>
                 <br/>
                 <br/>
                 <TextareaAutosize
                     className='auto_height'
                     name='description'
-                    onChange ={(e)=>setDescription(e.target.value)}
+                    onChange={(e) => setDescription(e.target.value)}
                     placeholder="Description"
                     minRows={3}
                     maxRows={20}
