2b287bb9aba92430629d5d64159db13058db579b
Tanvir Reza Anik <72945451+Anikreza@users.noreply.github.com>
2021-11-25 04:49:49 +0600

    Revert "Dev"

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,6 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-
 }
